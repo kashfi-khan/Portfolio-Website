e18312225c4c92b166e9b60bac8a3d283713bcb7
Kashfi Khan <kashfikhan67@gmail.com>
2025-06-25 22:27:25 +0500

    linkedin link set

--- src/components/contactMeSection/ContactSocial.jsx
+++ src/components/contactMeSection/ContactSocial.jsx
@@ -6,7 +6,7 @@ import { FaInstagram } from "react-icons/fa";
 const ContactSocial = () => {
   return (
     <div className="flex gap-4">
-  <SingleContactSocial link="https://www.linkedin.com/in/kashfikhan1/" Icon={FaLinkedinIn} />
+  <SingleContactSocial link="https://www.linkedin.com/in/kashfi-khan-09ab6422a/" Icon={FaLinkedinIn} />
   <SingleContactSocial link="https://github.com/kashfi-khan" Icon={FiGithub} />
   <SingleContactSocial link="https://www.instagram.com/kashfikhan.js/" Icon={FaInstagram} />
 </div>
