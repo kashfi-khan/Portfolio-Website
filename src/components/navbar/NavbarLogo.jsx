5cc5da61a4f326d2f562055fed9e71929f6cb332
Kashfi Khan <kashfikhan67@gmail.com>
2026-07-30 10:44:16 +0500

    responsive set

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -1,10 +1,10 @@
 const NavbarLogo = () => {
   return (
     <div>
-      <h1 className="text-white  text-2xl sm:hidden md:block">
+      <h1 className="text-white text-2xl hidden md:block">
         Kashfi Khan
       </h1>
-      <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
+      <h1 className="text-white font-special font-extrabold text-4xl md:hidden">
         Khan
       </h1>
     </div>
